12e1883b274fadf5dec6d8abdbf614833d935d4c
mrcsbin <81237987+mrcsbin@users.noreply.github.com>
2024-03-26 18:55:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 192 ms, Memory: 53624 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, k, w[], v[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		w = new int[n + 1];
+		v = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			w[i] = Integer.parseInt(st.nextToken());
+			v[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[][] dp = new int[n + 1][k + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= k; j++) {
+				dp[i][j] = dp[i - 1][j];
+				if (j - w[i] >= 0) {
+					dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
+				}
+			}
+		}
+		System.out.println(dp[n][k]);
+	}
+}
