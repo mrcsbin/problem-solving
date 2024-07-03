afa022832f5ca847556c1b014f75abb1ffd4c9ab
mrcsbin <81237987+mrcsbin@users.noreply.github.com>
2024-07-03 17:07:00 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1364 ms, Memory: 76112 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] root;
+    static boolean[] visited;
+    static List<Integer>[] graph;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        graph = new ArrayList[n + 1];
+        root = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) continue;
+            dfs(i);
+        }
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(root[i]);
+        }
+    }
+
+    private static void dfs(int node) {
+        visited[node] = true;
+        for (int nextNode : graph[node]) {
+            if (!visited[nextNode]) {
+                dfs(nextNode);
+                root[nextNode] = node;
+            }
+        }
+    }
+}
