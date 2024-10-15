cfae4868ba52bd15ae0d564559902e9a15ad437e
Seongbin Cho <81237987+mrcsbin@users.noreply.github.com>
2024-10-15 17:31:13 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 1264 ms, Memory: 144620 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -1,50 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     static int m, n;
-    static int[][] space, compressed;
-    static Map<Integer, Integer> rankMap;
+    static int[][] space;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-        space = new int[m][n];
-        compressed = new int[m][n];
+        space = new int[m][];
 
         for (int i = 0; i < m; i++) {
             space[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            compressed[i] = compress(space[i]);
+            compress(space[i]);
         }
 
         int answer = 0;
         for (int i = 0; i < m; i++) {
             for (int j = i + 1; j < m; j++) {
-                if (Arrays.equals(compressed[i], compressed[j])) answer++;
+                if (Arrays.equals(space[i], space[j])) answer++;
             }
         }
+        
         System.out.println(answer);
     }
 
-    private static int[] compress(int[] arr) {
-        int[] sorted = Arrays.stream(arr.clone()).sorted().toArray();
-        rankMap = new HashMap<>();
-
-        int idx = 0;
-        for (int value : sorted) {
-            rankMap.put(value, idx++);
-        }
-
+    private static void compress(int[] space) {
+        int[] sorted = Arrays.stream(space.clone()).sorted().toArray();
         for (int i = 0; i < n; i++) {
-            arr[i] = rankMap.get(arr[i]);
+            space[i] = Arrays.binarySearch(sorted, space[i]);
         }
-        return arr;
     }
 }
