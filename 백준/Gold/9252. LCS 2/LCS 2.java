d851503afaa2bbfc9022797db4111d44db79b362
Seongbin Cho <81237987+mrcsbin@users.noreply.github.com>
2024-12-05 16:47:19 +0900

    [Gold IV] Title: LCS 2, Time: 148 ms, Memory: 18260 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] a = br.readLine().toCharArray();
+        char[] b = br.readLine().toCharArray();
+        int[][] dp = new int[a.length + 1][b.length + 1];
+
+        for (int i = 1; i <= a.length; i++) {
+            for (int j = 1; j <= b.length; j++) {
+                if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        System.out.println(dp[a.length][b.length]);
+
+        StringBuilder sb = new StringBuilder();
+        int i = a.length;
+        int j = b.length;
+        while (i > 0 && j > 0) {
+            if (dp[i][j] == dp[i - 1][j]) i--;
+            else if (dp[i][j] == dp[i][j - 1]) j--;
+            else {
+                sb.append(a[i - 1]);
+                i--;
+                j--;
+            }
+        }
+        System.out.println(sb.reverse().toString());
+    }
+}
