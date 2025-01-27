57359c8a081a26f64c98e7a137e4acccef200e2e
Seongbin Cho <81237987+mrcsbin@users.noreply.github.com>
2025-01-27 17:16:55 +0900

    [Gold III] Title: 치즈, Time: 240 ms, Memory: 18664 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -1,17 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[][] map, tempMap;
-    static boolean[][] isOutside;
+    static int[][] map;
+    static List<Cheese> cheeses;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+
+    static class Cheese {
+        int x;
+        int y;
+        int outsideAirCount;
+
+        Cheese(int x, int y) {
+            this.x = x;
+            this.y = y;
+            getOutsideAirCount();
+        }
+
+        void getOutsideAirCount() {
+            int outsideAirCount = 0;
+            for (int d = 0; d < 4; d++) {
+                int nx = this.x + dx[d];
+                int ny = this.y + dy[d];
+                if (map[nx][ny] == -1) outsideAirCount++;
+            }
+
+            this.outsideAirCount = outsideAirCount;
+        }
+
+        void spreadAir() {
+            findOutsideAir(this.x, this.y);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,42 +44,56 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        tempMap = new int[n][m];
-
         for (int i = 0; i < n; i++) {
             map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        int time = -1;
-        while (true) {
-            time++;
-            findOutsideAir();
-            for (int i = 0; i < n; i++) {
-                tempMap[i] = map[i].clone();
+        findOutsideAir(0, 0);
+
+        cheeses = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 1) {
+                    cheeses.add(new Cheese(i, j));
+                }
             }
-            if (!melt()) break;
+        }
+
+        int time = 0;
+        while (!cheeses.isEmpty()) {
+            time++;
+
+            cheeses.sort((o1, o2) -> o2.outsideAirCount - o1.outsideAirCount);
+            for (int i = 0; i < cheeses.size(); i++) {
+                Cheese cheese = cheeses.get(i);
+                if (cheese.outsideAirCount >= 2) {
+                    cheese.spreadAir();
+                    cheeses.remove(i--);
+                    continue;
+                }
+                cheese.getOutsideAirCount();
+            }
         }
 
         System.out.println(time);
     }
 
-    private static void findOutsideAir() {
+    private static void findOutsideAir(int x, int y) {
         Queue<int[]> queue = new ArrayDeque<>();
-        isOutside = new boolean[n][m];
-        queue.offer(new int[]{0, 0});
-        isOutside[0][0] = true;
+        queue.offer(new int[]{x, y});
+        map[x][y] = -1;
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            x = current[0];
+            y = current[1];
 
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
-                if (isIn(nx, ny) && !isOutside[nx][ny] && map[nx][ny] == 0) {
+                if (isIn(nx, ny) && map[nx][ny] == 0) {
                     queue.offer(new int[]{nx, ny});
-                    isOutside[nx][ny] = true;
+                    map[nx][ny] = -1;
                 }
             }
         }
@@ -63,25 +101,5 @@ public class Main {
 
     private static boolean isIn(int x, int y) {
         return x >= 0 && y >= 0 && x < n && y < m;
-    }
-
-    private static boolean melt() {
-        boolean isCheeseLeft = false;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (tempMap[i][j] == 1) {
-                    isCheeseLeft = true;
-                    int outsideAir = 0;
-                    for (int d = 0; d < 4; d++) {
-                        int nx = i + dx[d];
-                        int ny = j + dy[d];
-                        if (isOutside[nx][ny]) outsideAir++;
-                    }
-
-                    if (outsideAir >= 2) map[i][j] = 0;
-                }
-            }
-        }
-        return isCheeseLeft;
     }
 }
