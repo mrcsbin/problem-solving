6c04565fc340fd429bff84a790b839867c605871
Seongbin Cho <81237987+mrcsbin@users.noreply.github.com>
2025-03-25 11:38:29 +0900

    [Gold IV] Title: 알고스팟, Time: 116 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static int[][] map, dist;
+    static int[][] map;
+    static int[][] dist;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
@@ -14,6 +16,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
+
         map = new int[n][m];
         dist = new int[n][m];
         for (int i = 0; i < n; i++) {
@@ -23,30 +26,33 @@ public class Main {
                 dist[i][j] = Integer.MAX_VALUE;
             }
         }
-        dijkstra();
+
+        bfs();
+
         System.out.println(dist[n - 1][m - 1]);
     }
 
-    private static void dijkstra() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
-        pq.offer(new int[]{0, 0, 0});
+    private static void bfs() {
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.offer(new int[]{0, 0});
         dist[0][0] = 0;
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int d = cur[2];
-            if (dist[cur[0]][cur[1]] < cur[2]) continue;
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        while (!deque.isEmpty()) {
+            int[] current = deque.poll();
+            int x = current[0];
+            int y = current[1];
+
+            if (x == n - 1 && y == m - 1) return;
+
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
                 if (isIn(nx, ny)) {
-                    if (map[nx][ny] == 1 && dist[nx][ny] > dist[x][y] + 1) {
-                        dist[nx][ny] = dist[x][y] + 1;
-                        pq.offer(new int[]{nx, ny, d + 1});
-                    } else if (map[nx][ny] == 0 && dist[nx][ny] > dist[x][y]) {
-                        dist[nx][ny] = dist[x][y];
-                        pq.offer(new int[]{nx, ny, d});
+                    int cost = map[nx][ny] == 0 ? 0 : 1;
+                    if (dist[nx][ny] > dist[x][y] + cost) {
+                        dist[nx][ny] = dist[x][y] + cost;
+                        if (cost == 0) deque.offerFirst(new int[]{nx, ny});
+                        else if (cost == 1) deque.offerLast(new int[]{nx, ny});
                     }
                 }
             }
